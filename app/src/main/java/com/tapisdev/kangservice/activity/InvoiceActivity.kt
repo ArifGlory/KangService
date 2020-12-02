6069235de3849c804b6dade20487772a59eaa56f
Arif Glory <arifglory46@gmail.com>
2020-12-02 15:17:55 +0700

    nambahin fungsi invoice

--- app/src/main/java/com/tapisdev/kangservice/activity/InvoiceActivity.kt
+++ app/src/main/java/com/tapisdev/kangservice/activity/InvoiceActivity.kt
@@ -1,81 +1,80 @@
-package com.tapisdev.kangservice.activity.pengguna
+package com.tapisdev.kangservice.activity
 
-import android.Manifest
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import com.tapisdev.cateringtenda.base.BaseActivity
 import com.tapisdev.kangservice.R
-import com.tapisdev.kangservice.activity.InvoiceActivity
 import com.tapisdev.kangservice.adapter.AdapterDetailPesanan
-import com.tapisdev.kangservice.adapter.AdapterSparepart
 import com.tapisdev.kangservice.model.CartSparepart
 import com.tapisdev.kangservice.model.Pesanan
-import com.tapisdev.kangservice.model.Sparepart
 import com.tapisdev.kangservice.model.UserModel
-import com.tapisdev.kangservice.util.PermissionHelper
-import kotlinx.android.synthetic.main.activity_detail_toko.*
 import kotlinx.android.synthetic.main.activity_detail_transaksi_sparepart.*
-import kotlinx.android.synthetic.main.activity_detail_transaksi_sparepart.ivBack
-import java.io.Serializable
+import kotlinx.android.synthetic.main.activity_invoice.*
 import java.text.DecimalFormat
 import java.text.NumberFormat
-import java.text.SimpleDateFormat
 import java.util.*
 
-class DetailTransaksiSparepartActivity : BaseActivity(){
+class InvoiceActivity : BaseActivity() {
 
     lateinit var i : Intent
     lateinit var pesanan : Pesanan
     lateinit var toko : UserModel
-
+    lateinit var user : UserModel
 
     var listCart = java.util.ArrayList<CartSparepart>()
     lateinit var adapter: AdapterDetailPesanan
-
     var TAG_GET_DETAILPESANAN = "detailpesananGET"
-
     var TAG_GET_USER = "penyediaGET"
-    var fotoBitmap : Bitmap? = null
-    private var fileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_transaksi_sparepart)
+        setContentView(R.layout.activity_invoice)
 
         i = intent
         pesanan = i.getSerializableExtra("pesanan") as Pesanan
         Log.d("pesanan"," id : "+pesanan.pesananId)
 
         adapter = AdapterDetailPesanan(listCart)
-        rvDetailPesanan.setHasFixedSize(true)
-        rvDetailPesanan.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
-        rvDetailPesanan.adapter = adapter
-
-        ivBack.setOnClickListener {
-            onBackPressed()
-        }
-        tvBuktiBayar.setOnClickListener {
-            val i = Intent(this,BuktiBayarActivity::class.java)
-            i.putExtra("pesanan",pesanan as Serializable)
-            startActivity(i)
-        }
-        tvKeInvoice.setOnClickListener {
-            val i = Intent(this,InvoiceActivity::class.java)
-            i.putExtra("pesanan",pesanan as Serializable)
-            startActivity(i)
-        }
+        rvDetailPesananInvoice.setHasFixedSize(true)
+        rvDetailPesananInvoice.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
+        rvDetailPesananInvoice.adapter = adapter
 
         updateUI()
+        getDataUser()
         getDataToko()
         getDataPesanan()
+    }
+
+    fun updateUI(){
+        tvTanggalNota.setText("Tanggal pesan : "+pesanan.tanggalPesan?.let { convertDate(it) })
+        tvAlamatInvoice.setText(""+pesanan.alamat)
+        tvKodePesanan.setText("#"+pesanan.pesananId!!.take(8))
+    }
+
+    fun getDataUser(){
+        userRef.document(pesanan.idUser.toString()).get().addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val document = task.result
+                if (document != null) {
+                    if (document.exists()) {
+                        Log.d(TAG_GET_USER, "DocumentSnapshot data: " + document.data)
+                        //convert doc to object
+                        user = document.toObject(UserModel::class.java)!!
+                        tvNamaPemesan.setText(user.name)
+
+                    } else {
+                        Log.d(TAG_GET_USER, "No such document")
+                    }
+                }
+            }else{
+                showErrorMessage("Pengguna tidak ditemukan")
+                Log.d(TAG_GET_USER,"err : "+task.exception)
+            }
+        }
     }
 
     fun getDataToko(){
@@ -87,8 +86,7 @@ class DetailTransaksiSparepartActivity : BaseActivity(){
                         Log.d(TAG_GET_USER, "DocumentSnapshot data: " + document.data)
                         //convert doc to object
                         toko = document.toObject(UserModel::class.java)!!
-                        tvToko.setText(toko.name)
-                        tvDeskripsiPenyedia.setText(toko.deskripsi)
+                        tvNamaToko.setText(toko.name)
 
                     } else {
                         Log.d(TAG_GET_USER, "No such document")
@@ -132,13 +130,7 @@ class DetailTransaksiSparepartActivity : BaseActivity(){
                 totalPrice += subtotal
             }
         }
-        tvTotalPrice.setText("Total Bayar : Rp. "+df.format(totalPrice))
-    }
-
-    fun updateUI(){
-        tvTanggal.setText("Tanggal pesan : "+pesanan.tanggalPesan?.let { convertDate(it) })
-        tvAlamat.setText("Alamat : "+pesanan.alamat)
-        tvStatus.setText(pesanan.status)
+        tvTotalPriceInvoice.setText("Rp. "+df.format(totalPrice))
     }
 
 }
